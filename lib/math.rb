26cd3c99ebdc8fdd33bc86eb36427d7d45aad3f8
YUKIKO IKEDA <applelele@gmail.com>
2020-01-19 18:40:03 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,12 +9,14 @@ end
 subtraction(20, 11)
 
 def division(num1, num2)
-d
+divs = num2 / num1
 end
+division(15, 3)
 
 def multiplication(num1, num2)
-
+multis = num1 * num2
 end
+multiplication(3, 5)
 
 def modulo(num1, num2)
 
